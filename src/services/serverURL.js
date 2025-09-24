876e2b19fdb12ae7fb36354ff9cf862d0dd671b0
milan721 <milanabrahamthomas@gmail.com>
2025-09-24 06:37:33 +0530

    v3

--- src/services/serverURL.js
+++ src/services/serverURL.js
@@ -1,2 +1,2 @@
-export const serverURL = 'https://rbuilder-backend-1-8n0f.onrender.com'
+export const serverURL = 'https://rbuilder-backend-2-fcld.onrender.com'
 // for api call in general remove the last forward / from the url. its done to avoid url changing during deploying. also we need to export it to be used by the other elements.
